920db740a8ef30ddfe23223a924646836532cad1
key5771 <ksj57715@hanmail.net>
2021-01-26 15:24:59 +0900

    range operator

--- RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
@@ -57,3 +57,14 @@ example(of: "never") {
         print("Completed")
     })
 }
+
+example(of: "range") {
+    let observable = Observable<Int>.range(start: 1, count: 10)
+    
+    observable.subscribe(onNext: { i in
+        let n = Double(i)
+        let fibonacci = Int(((pow(1.61803, n) - pow(0.61803, n)) / 2.23606).rounded())
+        
+        print(fibonacci)
+    })
+}
