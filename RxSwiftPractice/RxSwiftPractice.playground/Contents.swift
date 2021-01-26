36050f326d3959e834ff51c3bbad849b84d20813
key5771 <ksj57715@hanmail.net>
2021-01-26 14:49:05 +0900

    make playground & make Observable

---
+++ RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+import RxSwift
+
+public func example(of description: String, action: () -> Void) {
+    print("\n--- Example of:", description, "---")
+    action()
+}
+
+example(of: "just, of, from") {
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    let observable: Observable<Int> = Observable<Int>.just(one)
+    
+    let observable2 = Observable.of(one, two, three)
+    
+    let observable3 = Observable.of([one, two, three])
+    
+    let observable4 = Observable.from([one, two, three])
+}
+
+example(of: "subscribe") {
+    let one = 1
+    let two = 2
+    let three = 3
+    
+    let observable = Observable.of(one, two, three)
+    observable.subscribe { event in
+//        print(event) // next(1), next(2), next(3) 출력
+        if let element = event.element {
+            print(element) // 1, 2, 3 출력
+        }
+    }
+}
