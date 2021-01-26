5dc1f1993506e017c49aaa0309800f97f75181ea
key5771 <ksj57715@hanmail.net>
2021-01-26 15:29:53 +0900

    dispose & DisposeBag

--- RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
@@ -68,3 +68,20 @@ example(of: "range") {
         print(fibonacci)
     })
 }
+
+example(of: "dispose") {
+    let observable = Observable.of("A", "B", "C")
+    let subscription = observable.subscribe { event in
+        print(event)
+    }
+    
+    subscription.dispose()
+}
+
+example(of: "DisposeBag") {
+    let disposeBag = DisposeBag()
+    
+    Observable.of("A", "B", "C").subscribe {
+        print($0)
+    }.disposed(by: disposeBag)
+}
