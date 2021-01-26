cbbe10a779d8562b081fbcca69fa606e1129cad1
key5771 <ksj57715@hanmail.net>
2021-01-26 15:39:24 +0900

    create operator

--- RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
+++ RxSwiftPractice/RxSwiftPractice.playground/Contents.swift
@@ -85,3 +85,22 @@ example(of: "DisposeBag") {
         print($0)
     }.disposed(by: disposeBag)
 }
+
+example(of: "create") {
+    let disposeBag = DisposeBag()
+    
+    Observable<String>.create { observer in
+        observer.onNext("1")
+        
+        observer.onCompleted()
+        
+        observer.onNext("?")
+        
+        return Disposables.create()
+    }.subscribe(
+        onNext: { print($0) },
+        onError: { print($0) },
+        onCompleted: { print("Completed") },
+        onDisposed: { print("Disposed") }
+    ).disposed(by: disposeBag)
+}
